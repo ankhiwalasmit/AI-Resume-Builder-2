f65123ac3f9bc17534ffe43b2f55d72f001cf724
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-10-01 12:49:32 +0530

    fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -12,37 +12,37 @@ import ViewResume from './my-resume/[resumeId]/view/index.jsx'
 import ResumePreview from './dashboard/resume/components/ResumePreview.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/',
-    element:<Home/>
+    path: '/',
+    element: <Home />
   },
   {
-    element:<App/>,
-    children:[
+    element: <App />,
+    children: [
       {
-        path:'/dashboard',
-        element:<Dashboard/>
+        path: '/dashboard',
+        element: <Dashboard />
       },
       {
-        path:'/dashboard/resume/:resumeId/edit',
-        element:<EditResume/>
+        path: '/dashboard/resume/:resumeId/edit',
+        element: <EditResume />
       },
+      {
+        path: 'dashboard/resume/:resumeId/view',
+        element: <ResumePreview />
+      }
     ]
   },
   {
-    path:'/auth/sign-in',
-    element:<SignInPage/>
+    path: '/auth/sign-in',
+    element: <SignInPage />
   },
-  {
-    path:'/my-resume/:resumeId/view',
-    element:<ResumePreview />
-  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <RouterProvider router={router} />
     </ClerkProvider>
   </React.StrictMode>
