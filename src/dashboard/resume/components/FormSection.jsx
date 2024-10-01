a39a9c9be3d389731cb8aadc6a9a5fd74f4affbe
ankhiwalasmit <ankhiwalasmit@gmail.com>
2024-10-01 13:04:09 +0530

    tenth commit

--- src/dashboard/resume/components/FormSection.jsx
+++ src/dashboard/resume/components/FormSection.jsx
@@ -8,7 +8,6 @@ import Education from './forms/Education';
 import Skills from './forms/Skills';
 import { Link, Navigate, useParams } from 'react-router-dom';
 import ThemeColor from './ThemeColor';
-import ViewResume from '@/my-resume/[resumeId]/view';
 
 function FormSection() {
   const [activeFormIndex,setActiveFormIndex]=useState(1);
@@ -48,7 +47,7 @@ function FormSection() {
           :activeFormIndex==5?
           <Skills/>
           :activeFormIndex==6?
-          <ViewResume />
+          <Navigate to={'/my-resume/'+resumeId+"/view"}/>
               
         :null
           }
